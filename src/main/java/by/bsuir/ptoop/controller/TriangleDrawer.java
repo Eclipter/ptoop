d82b8e0b6b82075157be5e7f699e9466183ddb1d
Vladislav Dektiarev <eclipter97@gmail.com>
2018-03-24 12:10:54 +0300

    Draw all points of Triangle instead of only one

--- src/main/java/by/bsuir/ptoop/controller/TriangleDrawer.java
+++ src/main/java/by/bsuir/ptoop/controller/TriangleDrawer.java
@@ -19,11 +19,11 @@ public class TriangleDrawer extends AbstractDrawer {
         double[] xCoords = new double[3];
         double[] yCoords = new double[3];
         xCoords[0] = triangle.getA().getX();
-        xCoords[1] = triangle.getA().getX();
-        xCoords[2] = triangle.getA().getX();
+        xCoords[1] = triangle.getB().getX();
+        xCoords[2] = triangle.getC().getX();
         yCoords[0] = triangle.getA().getY();
-        yCoords[1] = triangle.getA().getY();
-        yCoords[2] = triangle.getA().getY();
+        yCoords[1] = triangle.getB().getY();
+        yCoords[2] = triangle.getC().getY();
         getGraphicsContext().fillPolygon(xCoords, yCoords, 3);
     }
 }
