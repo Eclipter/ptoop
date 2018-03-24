8993354bc41f425d402424ea27c290ea4468fc1a
Vladislav Dektiarev <eclipter97@gmail.com>
2018-03-24 11:02:38 +0300

    Update AbstractDrawer to follow ChainOfResponsibility pattern

--- src/main/java/by/bsuir/ptoop/controller/AbstractDrawer.java
+++ src/main/java/by/bsuir/ptoop/controller/AbstractDrawer.java
@@ -1,5 +1,32 @@
 package by.bsuir.ptoop.controller;
 
+import by.bsuir.ptoop.model.Figure;
+
 public abstract class AbstractDrawer
 {
+    private AbstractDrawer nextDrawer;
+
+    public abstract boolean supports(Figure figure);
+
+    public void draw(Figure figure)
+    {
+        if(!supports(figure))
+        {
+            nextDrawer.draw(figure);
+        }
+        else
+        {
+            drawFigure(figure);
+        }
+    }
+
+    public abstract void drawFigure(Figure figure);
+
+    public AbstractDrawer getNextDrawer() {
+        return nextDrawer;
+    }
+
+    public void setNextDrawer(AbstractDrawer nextDrawer) {
+        this.nextDrawer = nextDrawer;
+    }
 }
